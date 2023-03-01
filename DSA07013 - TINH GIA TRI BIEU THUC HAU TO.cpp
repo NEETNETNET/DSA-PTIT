33e2f3a4d8eab26e027b90c146ff4bcac5d809fc
NEETNETNET <112066731+NEETNETNET@users.noreply.github.com>
2023-03-01 22:53:45 +0700

    C++

---
+++ DSA07013 - TINH GIA TRI BIEU THUC HAU TO.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+#define fort(i,a,b) for(int i=a;i<b;++i)
+#define forg(i,a,b) for(int i=a;i>b;--i)
+#define fort1(i,a,b) for(int i=a;i<=b;++i)
+#define forg1(i,a,b) for(int i=a;i>=b;--i)
+#define t() int t; cin >> t; while(t--)
+#define faster() cin.tie(0); ios_base::sync_with_stdio(false); cout.tie(0);
+#define pb push_back
+
+
+int check(string a){
+	stack<int> s;
+	fort1(i,0,a.length()-1){
+		if(a[i]>='0'&&a[i]<='9') s.push(a[i]-'0');
+		else {
+			int b=s.top();
+			s.pop();
+			int c= s.top();
+			s.pop();
+			if(a[i]=='+') s.push(b+c);
+			if(a[i]=='-') s.push(c-b);
+			if(a[i]=='*') s.push(b*c);
+			if(a[i]=='/') s.push(c/b);
+		}
+	}return s.top();
+}
+int main(){
+	t(){
+		string s;
+		cin >> s;
+		cout << check(s) << endl;
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
